a34bea8fce07dbb1da42363fc2ae860ce647fa90
Tom Brewer <tombrewer0@gmail.com>
2026-03-01 23:33:30 -0500

    delete some stuff

--- Iris.Cloud.Demo.Contracts/ChangeColorsCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Iris.Cloud.Demo.Contracts
-{
-    public record ChangeColorsCommand(int Red, int Green, int Blue);
-}
-
--- Iris.Cloud.Demo.Contracts/ChangeColorsCommandV2.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Iris.Cloud.Demo.Contracts
-{
-    public record ChangeColorsCommandV2(int Hue, float Saturation, float Light);
-
-}
-
