a146c1829986d5421b8acbf30228d9aa5157d4d6
Tom Brewer <tombrewer0@gmail.com>
2026-04-09 21:50:22 -0600

    feat: framework auto update configuration

---
+++ Iris.Desktop/Configuration/IrisDownloadConfiguration.cs
@@ -0,0 +1,6 @@
+namespace Iris.Desktop.Configuration;
+
+public static class IrisDownloadConfiguration
+{
+    public const string UpdateBaseUrl = "https://stmythetechglobal.blob.core.windows.net/releases/iris";
+}
