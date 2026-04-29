3f4a0c759e442dc0ca6de0a62ca053ad3c3d28bb
Tom Brewer <tombrewer0@gmail.com>
2026-04-28 22:41:05 -0600

    update packages, fix settins dialog button

---
+++ Iris.Desktop/Configuration/HermesLicense.cs
@@ -0,0 +1,6 @@
+namespace Iris.Desktop.Configuration;
+
+public static class HermesLicense
+{
+    public const string Key = "HERMES-1-AQIW4zh2p_DfQbzOQGbpS2MPVAhbqf____9lADx6GGQw6Gnxh2qyoWlKoxR5s677RoR1LT8BK9fAkn9SozqMDxd1PkjTj1cBE6_4Kt4TZbrPdgXyvpRtf-FtxelEFExAyeVn3Lx7";
+}
